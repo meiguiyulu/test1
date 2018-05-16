1d38b7693abd8d59b9ad155ab84631a2618a5ad9
Excilbur <38106024+Excilbur@users.noreply.github.com>
2018-05-16 19:55:28 +0800

    Initial commit

---
+++ Vector.h
@@ -0,0 +1,38 @@
+// Vector.h: interface for the CVector class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_VECTOR_H__4E734614_050F_4B11_AB73_405B18FA14EE__INCLUDED_)
+#define AFX_VECTOR_H__4E734614_050F_4B11_AB73_405B18FA14EE__INCLUDED_
+#include "P3.h"
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+class CVector  
+{
+public:
+	CVector();
+	virtual ~CVector();
+	CVector(double x,double y,double z);
+	CVector(const CP3 &);
+	CVector(const CP3 &,const CP3 &);
+	double Mag();//矢量的模
+	CVector Normalize();//单位矢量
+	friend CVector operator +(const CVector &,const CVector &);
+	friend CVector operator -(const CVector &,const CVector &);
+	friend CVector operator *(const CVector &,double);
+	friend CVector operator *(double,const CVector &);
+	friend CVector operator /(const CVector &,double);
+	friend CVector operator +=(CVector &,CVector &);
+	friend CVector operator -=(CVector &,CVector &);
+	friend CVector operator *=(CVector &,CVector &);
+	friend CVector operator /=(CVector &,double);
+	friend double Dot(const CVector &,const CVector &);//点乘
+	friend CVector Cross(const CVector &,const CVector &);//叉乘
+public:
+	double x,y,z;
+};
+
+#endif // !defined(AFX_VECTOR_H__4E734614_050F_4B11_AB73_405B18FA14EE__INCLUDED_)
